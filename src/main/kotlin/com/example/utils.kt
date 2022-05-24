1406c264f79a8fe62086aebb491a8df56a1ce093
Davenury <dawid199960@gmail.com>
2022-05-24 19:01:27 +0200

    httpClient timeout

--- src/main/kotlin/com/example/utils.kt
+++ src/main/kotlin/com/example/utils.kt
@@ -3,11 +3,15 @@ package com.example
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
+import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 
 val objectMapper = jacksonObjectMapper()
 val httpClient = HttpClient(OkHttp) {
     install(JsonFeature) {
         serializer = JacksonSerializer(objectMapper)
+    }
+    install(HttpTimeout) {
+        requestTimeoutMillis = 10000
     }
 }
