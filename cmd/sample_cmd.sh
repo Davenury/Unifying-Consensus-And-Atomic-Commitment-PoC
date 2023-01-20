991c93e63b305009ef8993300ca49101a0c68484
Davenury <dawid199960@gmail.com>
2023-01-20 15:49:38 +0100

    dashboard

--- cmd/sample_cmd.sh
+++ cmd/sample_cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./ucac perform --monitoring-namespace=ddebowski \
---peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:c9014fe6e9c8293a33d0adadf19ac48dc462d8a3 \
---performance-test-image=ghcr.io/davenury/tests:c9014fe6e9c8293a33d0adadf19ac48dc462d8a3 --single-requests-number=10000 --multiple-requests-number=20000 \
+--peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:300f11428969012012e6f4b820bc89fccd5fde22 \
+--performance-test-image=ghcr.io/davenury/tests:300f11428969012012e6f4b820bc89fccd5fde22 --single-requests-number=4000 --multiple-requests-number=5000 \
 --test-duration=PT10M --max-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
---ac-protocol=gpac --performance-test-timeout-deadline=PT120M
+--ac-protocol=two_pc --enforce-ac --performance-test-timeout-deadline=PT120M
