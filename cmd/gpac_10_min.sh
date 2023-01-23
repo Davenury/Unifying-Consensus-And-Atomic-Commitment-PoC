3c1b197c298e1f0dec3686bcb88b38c1e9d1d201
Davenury <dawid199960@gmail.com>
2023-01-23 12:43:52 +0100

    change image

--- cmd/gpac_10_min.sh
+++ cmd/gpac_10_min.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./ucac perform --monitoring-namespace=ddebowski \
---peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:285a28dcd8be1fa0a5168265842ada781e6f29c2 \
---performance-test-image=ghcr.io/davenury/tests:285a28dcd8be1fa0a5168265842ada781e6f29c2 --single-requests-number=0 --multiple-requests-number=3600 \
+--peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:3ee83256e95b31638507874823d9eb77b4a7bce4 \
+--performance-test-image=ghcr.io/davenury/tests:3ee83256e95b31638507874823d9eb77b4a7bce4 --single-requests-number=0 --multiple-requests-number=3600 \
 --test-duration=PT10M --max-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
 --ac-protocol=gpac --enforce-ac --performance-test-timeout-deadline=PT120M
