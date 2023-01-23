c09dddd03e72be5ae8eb3ada6e14b06d59748bdc
Davenury <dawid199960@gmail.com>
2023-01-23 12:32:06 +0100

    increase timeout

--- cmd/gpac_10_min.sh
+++ cmd/gpac_10_min.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./ucac perform --monitoring-namespace=ddebowski \
---peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:7221bd01d091837c6a94c34d032198c31bfafa6c \
---performance-test-image=ghcr.io/davenury/tests:7221bd01d091837c6a94c34d032198c31bfafa6c --single-requests-number=0 --multiple-requests-number=3600 \
+--peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:285a28dcd8be1fa0a5168265842ada781e6f29c2 \
+--performance-test-image=ghcr.io/davenury/tests:285a28dcd8be1fa0a5168265842ada781e6f29c2 --single-requests-number=0 --multiple-requests-number=3600 \
 --test-duration=PT10M --max-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
 --ac-protocol=gpac --enforce-ac --performance-test-timeout-deadline=PT120M
--- cmd/sample_cmd.sh
+++ cmd/sample_cmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./ucac perform --monitoring-namespace=ddebowski \
---peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:977001d20872527024cf8f4b02c211537a4d9cbc \
---performance-test-image=ghcr.io/davenury/tests:977001d20872527024cf8f4b02c211537a4d9cbc --single-requests-number=0 --multiple-requests-number=1000 \
+--peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:285a28dcd8be1fa0a5168265842ada781e6f29c2 \
+--performance-test-image=ghcr.io/davenury/tests:285a28dcd8be1fa0a5168265842ada781e6f29c2 --single-requests-number=0 --multiple-requests-number=1000 \
 --test-duration=PT1M --max-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
 --ac-protocol=two_pc --performance-test-timeout-deadline=PT120M
