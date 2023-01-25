3bfd2ec98c22470d4c6000e0477f007d6324cde6
Davenury <dawid199960@gmail.com>
2023-01-25 22:06:05 +0100

    dashboard supports multiple namespaces

--- cmd/gpac_10_min.sh
+++ cmd/gpac_10_min.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./ucac perform --monitoring-namespace=ddebowski \
---peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:4c4369b1a44cbedf179fe7d2d2bcdc7ecadc122e \
---performance-test-image=ghcr.io/davenury/tests:4c4369b1a44cbedf179fe7d2d2bcdc7ecadc122e --constant-load=6 \
+--peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:36be4a2a6608eab4a2e134366d7a294c31bf6405 \
+--performance-test-image=ghcr.io/davenury/tests:36be4a2a6608eab4a2e134366d7a294c31bf6405 --constant-load=3 \
 --fixed-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
 --ac-protocol=gpac --enforce-ac --performance-test-timeout-deadline=PT120M
