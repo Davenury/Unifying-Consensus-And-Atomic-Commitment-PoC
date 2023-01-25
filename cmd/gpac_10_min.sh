1cb7b049037e14a094a33faa3b3fdc14d55a6495
Davenury <dawid199960@gmail.com>
2023-01-25 19:20:39 +0100

    new commands

--- cmd/gpac_10_min.sh
+++ cmd/gpac_10_min.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./ucac perform --monitoring-namespace=ddebowski \
---peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:936776953955857e1562a4dc2809f6b454cd242c \
---performance-test-image=ghcr.io/davenury/tests:936776953955857e1562a4dc2809f6b454cd242c --single-requests-number=0 --multiple-requests-number=1 \
---test-duration=PT10M --max-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
+--peers=$(python -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:bd89efd4bbbe7c6d664df591b16ae5f660b09ba9 \
+--performance-test-image=ghcr.io/davenury/tests:bd89efd4bbbe7c6d664df591b16ae5f660b09ba9 --constant-load=7 \
+--max-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
 --ac-protocol=gpac --enforce-ac --performance-test-timeout-deadline=PT120M
--- cmd/metrics_test.sh
+++ cmd/metrics_test.sh
@@ -3,4 +3,4 @@
 ./ucac perform --monitoring-namespace=ddebowski \
 --peers=3,3 --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:71bfdd0a4112b74c95ba429f4b38c1f979c28fa1 \
 --performance-test-image=ghcr.io/davenury/tests:71bfdd0a4112b74c95ba429f4b38c1f979c28fa1  --max-peersets-in-change=2 --tests-strategy=delay_on_conflicts \
---ac-protocol=gpac --performance-test-timeout-deadline=PT120M --is-metric-test --constant-load=1 --deploy-monitoring=false
+--ac-protocol=gpac --performance-test-timeout-deadline=PT120M --is-metric-test --constant-load=1
