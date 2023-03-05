a9eb66506541d3266c84152c66ad23a88c6eb5fc
Davenury <dawid199960@gmail.com>
2023-03-05 09:54:23 +0100

    why lag

--- src/main/kotlin/com/github/davenury/ucac/common/ChangeNotifier.kt
+++ src/main/kotlin/com/github/davenury/ucac/common/ChangeNotifier.kt
@@ -24,7 +24,6 @@ object ChangeNotifier {
                         sendNotification(notificationUrl, change, changeResult)
                     }
                 }
-                logger.info("Notifier accepted")
             }
         }
     }
