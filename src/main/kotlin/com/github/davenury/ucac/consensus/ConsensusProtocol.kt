9d688f07563485e4dd90204e0e050d68d3b6e930
Radosław Szuma <radoslawszuma@gmail.com>
2023-03-09 23:26:44 +0100

    POB layer almost done

--- src/main/kotlin/com/github/davenury/ucac/consensus/ConsensusProtocol.kt
+++ src/main/kotlin/com/github/davenury/ucac/consensus/ConsensusProtocol.kt
@@ -3,6 +3,7 @@ package com.github.davenury.ucac.consensus
 import com.github.davenury.common.Change
 import com.github.davenury.common.ChangeResult
 import com.github.davenury.common.history.History
+import com.github.davenury.ucac.common.PeerAddress
 import java.util.concurrent.CompletableFuture
 
 interface ConsensusProtocol {
@@ -16,4 +17,8 @@ interface ConsensusProtocol {
     fun getState(): History
 
     fun getChangeResult(changeId: String): CompletableFuture<ChangeResult>?
+
+    fun isMoreThanHalf(value: Int): Boolean = (value + 1) * 2 > otherConsensusPeers().size + 1
+
+    fun otherConsensusPeers(): List<PeerAddress>
 }
