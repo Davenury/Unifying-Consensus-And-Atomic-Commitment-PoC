88a181ceb2f4678fd1a7685d2aab222dfa58c6fc
Davenury <dawid199960@gmail.com>
2023-03-10 21:09:43 +0100

    timeout change if tests don't get notification

--- modules/tests/src/main/kotlin/com/github/davenury/tests/Changes.kt
+++ modules/tests/src/main/kotlin/com/github/davenury/tests/Changes.kt
@@ -7,9 +7,15 @@ import com.github.davenury.common.Notification
 import com.github.davenury.common.history.InitialHistoryEntry
 import com.github.davenury.tests.strategies.changes.CreateChangeStrategy
 import com.github.davenury.tests.strategies.peersets.GetPeersStrategy
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import org.slf4j.LoggerFactory
+import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicReference
 
 class Changes(
@@ -23,6 +29,7 @@ class Changes(
     private val handledChanges: MutableList<String> = mutableListOf()
     private val mutex = Mutex()
     private val notificationMutex = Mutex()
+    private val executor = Executors.newCachedThreadPool().asCoroutineDispatcher()
 
     suspend fun handleNotification(notification: Notification) = notificationMutex.withLock {
         logger.info("Handling notification: $notification")
@@ -40,7 +47,7 @@ class Changes(
     }
 
     suspend fun introduceChange(numberOfPeersets: Int) {
-        mutex.withLock {
+        val change = mutex.withLock {
             val ids = getPeersStrategy.getPeersets(numberOfPeersets)
 
             val change = createChangeStrategy.createChange(ids, changes)
@@ -51,6 +58,19 @@ class Changes(
             } else {
                 logger.info("Change $change was rejected, freeing peersets $ids")
                 getPeersStrategy.freePeersets(ids)
+            }
+            change
+        }
+        executor.dispatch(Dispatchers.IO) {
+            runBlocking {
+                delay(8000)
+                logger.error("Change $change timed out from performance tests, freeing peersets")
+                mutex.withLock {
+                    if (!handledChanges.contains(change.id)) {
+                        getPeersStrategy.freePeersets(change.peersets.map { it.peersetId })
+                        handledChanges.add(change.id)
+                    }
+                }
             }
         }
     }
