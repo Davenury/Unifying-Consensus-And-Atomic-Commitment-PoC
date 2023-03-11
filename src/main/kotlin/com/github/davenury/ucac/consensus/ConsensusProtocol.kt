b096ddfa397a03bdcf365375ae75dfdc2f5559f9
Radosław Szuma <radoslawszuma@gmail.com>
2023-03-11 14:30:27 +0100

    More wip

--- src/main/kotlin/com/github/davenury/ucac/consensus/ConsensusProtocol.kt
+++ src/main/kotlin/com/github/davenury/ucac/consensus/ConsensusProtocol.kt
@@ -4,6 +4,8 @@ import com.github.davenury.common.Change
 import com.github.davenury.common.ChangeResult
 import com.github.davenury.common.history.History
 import com.github.davenury.ucac.common.PeerAddress
+import com.github.davenury.ucac.common.ProtocolTimerImpl
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import java.util.concurrent.CompletableFuture
 
 interface ConsensusProtocol {
@@ -19,6 +21,7 @@ interface ConsensusProtocol {
     fun getChangeResult(changeId: String): CompletableFuture<ChangeResult>?
 
     fun isMoreThanHalf(value: Int): Boolean = (value + 1) * 2 > otherConsensusPeers().size + 1
+
 
     fun otherConsensusPeers(): List<PeerAddress>
 }
