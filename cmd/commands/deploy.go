288c600a9ad49dec58355132426fcbbd11c82a33
Davenury <dawid199960@gmail.com>
2023-03-11 14:10:40 +0100

    dashboard changes

--- cmd/commands/deploy.go
+++ cmd/commands/deploy.go
@@ -51,6 +51,7 @@ func CreateDeployCommand() *cobra.Command {
 	deployCommand.Flags().BoolVar(&config.CreateResources, "create-resources", true, "Determines if pods should have limits and requests")
 	deployCommand.Flags().StringVar(&config.ProxyDelay, "proxy-delay", "0", "Delay in seconds for proxy, e.g. 0.2")
 	deployCommand.Flags().StringVar(&config.ProxyLimit, "proxy-limit", "0", "Bandwidth limit in bytes per second, e.g. 100, 2M")
+	deployCommand.Flags().StringVar(&config.MonitoringNamespace, "monitoring-namespace", "ddebowski", "Namespace with monitoring deployed")
 
 	return deployCommand
 
