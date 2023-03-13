7c39a8ebea459ff2dc1eb11e2396dd32e3d32f8c
Davenury <dawid199960@gmail.com>
2023-03-13 14:25:53 +0100

    remove get accept val from superset

--- src/main/kotlin/com/github/davenury/ucac/commitment/gpac/GPACProtocol.kt
+++ src/main/kotlin/com/github/davenury/ucac/commitment/gpac/GPACProtocol.kt
@@ -278,7 +278,8 @@ class GPACProtocolImpl(
             val electResponses = electMeResult.responses
 
 
-            val acceptVal = electResponses.getAcceptVal(change)
+            val acceptVal =
+                if (electResponses.flatten().all { it.initVal == Accept.COMMIT }) Accept.COMMIT else Accept.ABORT
 
             this.transaction = this.transaction.copy(acceptVal = acceptVal, acceptNum = myBallotNumber)
 
@@ -301,13 +302,6 @@ class GPACProtocolImpl(
         } catch (e: Exception) {
             changeIdToCompletableFuture[change.id]!!.complete(ChangeResult(ChangeResult.Status.CONFLICT, e.message))
         }
-    }
-
-    private fun List<List<ElectedYou>>.getAcceptVal(change: Change): Accept {
-        val thresholds = getPeersFromChange(change).map { (it.size + 1) / 2 }
-        val responses = this.map { it.count { it.acceptVal == Accept.COMMIT } }
-
-        return if (responses.zip(thresholds).all { (res, threshold) -> res >= threshold }) Accept.COMMIT else Accept.ABORT
     }
 
     override suspend fun performProtocolAsRecoveryLeader(change: Change, iteration: Int) {
