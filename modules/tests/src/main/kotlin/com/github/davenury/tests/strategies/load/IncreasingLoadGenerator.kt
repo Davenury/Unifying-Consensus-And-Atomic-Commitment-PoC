f02a3dcdb765b6849b0f039e3e1dd3ba8601a8d9
Davenury <57959794+Davenury@users.noreply.github.com>
2023-03-17 22:50:22 +0100

    introduce stress tests (on peers, not students) (#138)
    * introduce stress tests
    
    * current expected load as metric
    
    * fixes
    
    * fix metric
    
    * fix

---
+++ modules/tests/src/main/kotlin/com/github/davenury/tests/strategies/load/IncreasingLoadGenerator.kt
@@ -0,0 +1,58 @@
+package com.github.davenury.tests.strategies.load
+
+import com.github.davenury.common.meterRegistry
+import com.github.davenury.tests.Metrics
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Gauge
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.ticker
+import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicReference
+import java.util.function.Supplier
+
+class IncreasingLoadGenerator(
+    private val bound: Double,
+    private val increaseDelay: Duration,
+    private val increaseStep: Double = 1.0,
+) : LoadGenerator {
+
+    private var channel: ReceiveChannel<Unit> = ticker(1000, 0)
+    private var currentTick: Double = 1.0
+
+    init {
+        Counter.builder("current_expected_load").register(meterRegistry).increment(currentTick)
+    }
+
+    override fun generate() {
+        ctx.dispatch(Dispatchers.IO) {
+            runBlocking {
+                while (currentTick <= bound) {
+                    delay(increaseDelay.toMillis())
+                    currentTick += increaseStep
+                    channel = ticker((1000 / currentTick).toLong(), 0)
+                    logger.info("Bumped load to $currentTick changes per second")
+                    Counter.builder("current_expected_load").register(meterRegistry).increment(increaseStep)
+                }
+            }
+        }
+    }
+
+    override suspend fun subscribe(fn: suspend () -> Unit) {
+        withContext(ctx) {
+            while (true) {
+                channel.receive()
+                fn()
+            }
+        }
+    }
+
+    override fun getName(): String = "IncreasingLoadGenerator"
+
+    companion object {
+        private val ctx = Executors.newCachedThreadPool().asCoroutineDispatcher()
+        private val logger = LoggerFactory.getLogger("IncreasingLoadGenerator")
+    }
+}
