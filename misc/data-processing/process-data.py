b9e3c97ec65ecc1721e25b9ba7b0612bb28fa924
Davenury <dawid199960@gmail.com>
2023-03-21 17:32:43 +0100

    data for boxplots

--- misc/data-processing/process-data.py
+++ misc/data-processing/process-data.py
@@ -4,9 +4,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 
-COLUMN_NAME = "changes per second"
-FILE_NAME = "changes_per_second"
+COLUMN_NAME = "avg commit latency"
+FILE_NAME = "network_bytes_received"
+new_file_name = "network-bytes-received-non-leaders"
 
+leaders = {
+    "gpac/3x2": ["peer0-peerset0", "peer0-peerset1"],
+    "gpac/4x2": ["peer0-peerset0", "peer0-peerset1"],
+    "gpac/5x2": ["peer0-peerset0", "peer0-peerset1"],
+    "gpac/6x2": ["peer0-peerset0", "peer0-peerset1"],
+    "gpac/10x2": ["peer0-peerset0", "peer0-peerset1"],
+    "gpac/20x2": ["peer0-peerset0", "peer0-peerset1"],
+    "raft/3x1": ["peer2-peerset0"],
+    "raft/4x1": ["peer0-peerset0"],
+    "raft/5x1": ["peer0-peerset0"],
+    "raft/6x1": ["peer5-peerset0"],
+    "raft/10x1": ["peer4-peerset0"],
+    "raft/20x1": ["peer19-peerset0"],
+    "2pc/3x2": ["peer1-peerset0", "peer0-peerset1"],
+    "2pc/4x2": ["peer1-peerset0", "peer3-peerset1"],
+    "2pc/5x2": ["peer3-peerset0", "peer0-peerset1"],
+    "2pc/6x2": ["peer4-peerset0", "peer5-peerset1"],
+    "2pc/10x2": ["peer5-peerset0", "peer4-peerset1"],
+    "2pc/20x2": ["peer4-peerset0", "peer0-peerset1"],
+}
+
+SWAP_LEADERS = True
+
+if SWAP_LEADERS:
+    leaders2 = {}
+    for name, leader in leaders.items():
+        swapped = ["-".join(l.split("-")[::-1]) for l in leader]
+        leaders2[name] = swapped
+    leaders = leaders2
+
+LEADERS = 0 # 0 - drop leaders, 1 - leave only leaders
 
 class Experiment:
     def __init__(self, name):
@@ -24,14 +56,33 @@ class Experiment:
         return np.nanmean(self.df.to_numpy()), np.nanstd(self.df.to_numpy())
 
     def calculate_box_plot_values(self):
+        self.df = self.df[self.df.columns.drop(list(self.df.filter(regex='nonheap')))]
+        self.df = self.df.dropna()
+
+        if LEADERS == 1:
+            self.leave_leaders()
+        if LEADERS == 0:
+            self.drop_leaders()
+
         bp = plt.boxplot(self.df.to_numpy().flatten())
         dict1 = {}
-        dict1['lower_whisker'] = bp['whiskers'][0].get_ydata()[1]
-        dict1['lower_quartile'] = bp['boxes'][0].get_ydata()[1]
+        dict1['lower-whisker'] = bp['whiskers'][0].get_ydata()[1]
+        dict1['lower-quartile'] = bp['boxes'][0].get_ydata()[1]
         dict1['median'] = bp['medians'][0].get_ydata()[1]
-        dict1['upper_quartile'] = bp['boxes'][0].get_ydata()[2]
-        dict1['upper_whisker'] = bp['whiskers'][1].get_ydata()[1]
+        dict1['upper-quartile'] = bp['boxes'][0].get_ydata()[2]
+        dict1['upper-whisker'] = bp['whiskers'][1].get_ydata()[1]
         return dict1
+
+    def leave_leaders(self):
+        regex = "|".join(leaders[self._inner_name()])
+        self.df = self.df.filter(regex=regex)
+
+    def drop_leaders(self):
+        for leader in leaders[self._inner_name()]:
+            self.df = self.df[self.df.columns.drop(list(self.df.filter(regex=leader)))]
+    def _inner_name(self):
+        split = self.name.split("/")
+        return f"{split[1]}/{split[2]}"
 
     def representative_name(self):
         split_name = self.name.split("/")
@@ -62,8 +113,7 @@ def prepare_experiments():
 
 def values_from_experiment(ex):
     return {
-        "protocol": ex[0].split(" - ")[0],
-        "number_of_peers": ex[0].split(" - ")[1].split(" ")[0],
+        "name": ex[0],
         **ex[1]
     }
 
@@ -107,21 +157,11 @@ def get_values():
     all = list(itertools.chain(gpac, raft, twopc))
     return [values_from_experiment(a) for a in all]
 
-def prepare_latex_code():
-    pass
 
-    # template = latex_template(all)
-    #
-    # latex = "\n".join([latex_from_experiment(ex) for ex in all])
-    #
-    # result = template.replace("<----PLOTS HERE---->", latex)
-    # print(result)
-
-
-def get_data_in_dat():
+def get_data_in_csv():
     values = get_values()
     df = pd.DataFrame.from_records(values)
-    df.to_csv(f"parsed_csvs/{FILE_NAME}.dat", sep="\t", index=False)
+    df.to_csv(f"parsed_csvs/{new_file_name}.csv", sep=",", index=False)
 
 # def plot_experiments():
 #     width = 0.2
@@ -143,4 +183,5 @@ def get_data_in_dat():
 
 
 if __name__ == "__main__":
-    get_data_in_dat()
+    get_data_in_csv()
+
