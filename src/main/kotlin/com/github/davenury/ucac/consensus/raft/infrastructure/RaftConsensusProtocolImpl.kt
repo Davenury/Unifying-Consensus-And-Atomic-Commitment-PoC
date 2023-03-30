26b0a82e0e6ba1458488c7a63629d1249748f159
Radosław Szuma <radoslawszuma@gmail.com>
2023-03-30 22:54:05 +0200

    Limit rate in sendRequestToLeader

--- src/main/kotlin/com/github/davenury/ucac/consensus/raft/infrastructure/RaftConsensusProtocolImpl.kt
+++ src/main/kotlin/com/github/davenury/ucac/consensus/raft/infrastructure/RaftConsensusProtocolImpl.kt
@@ -796,6 +796,7 @@ class RaftConsensusProtocolImpl(
                         logger.error("Request to leader ($address, ${votedFor?.id}) failed", e.message)
                         null
                     }
+                    if(result == null) delay(heartbeatDelay.toMillis())
                 }
                 if (result.status != ChangeResult.Status.SUCCESS) {
                     cf.complete(result)
