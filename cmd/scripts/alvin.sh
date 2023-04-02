23914aee5f892121bfcf2652cc06c561fb579bb2
Radosław Szuma <radoslawszuma@gmail.com>
2023-04-02 23:08:47 +0200

    Change alvin script

--- cmd/scripts/alvin.sh
+++ cmd/scripts/alvin.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 --performance-test-image=ghcr.io/davenury/tests:2fde267f12e1bfbdc05ecd163280dca8f62d8ad0 --constant-load=5 --load-generator-type=constant \
 --fixed-peersets-in-change=1 --tests-sending-strategy=delay_on_conflicts \
 --consensus-protocol=alvin \
---ac-protocol=two_pc --performance-test-timeout-deadline=PT120M --deploy-monitoring=true
+--ac-protocol=two_pc --performance-test-timeout-deadline=PT120M --deploy-monitoring=false
