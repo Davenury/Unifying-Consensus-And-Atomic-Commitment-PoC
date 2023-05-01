00a6514f93cf9ebcc89efd9bf61a7847c5a04507
Radosław Szuma <radoslawszuma@gmail.com>
2023-05-01 11:28:34 +0200

    Modify dashboard

--- cmd/scripts/consensus.sh
+++ cmd/scripts/consensus.sh
@@ -4,8 +4,8 @@ echo "Run protocol: $CONSENSUS"
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 "${SCRIPT_DIR}/../ucac" perform --monitoring-namespace=rszuma \
---peers=$(python3 -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:af66ef035a7e43eec35a34c39d10adca0c1bff6a  \
---performance-test-image=ghcr.io/davenury/tests:af66ef035a7e43eec35a34c39d10adca0c1bff6a --constant-load=5 --load-generator-type=constant \
+--peers=$(python3 -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:3b39fa5511dd67f08fa2c7eda4a1c1fdd01a61f8  \
+--performance-test-image=ghcr.io/davenury/tests:3b39fa5511dd67f08fa2c7eda4a1c1fdd01a61f8 --constant-load=5 --load-generator-type=constant \
 --fixed-peersets-in-change=1 --tests-sending-strategy=delay_on_conflicts \
 --consensus-protocol=${CONSENSUS:-raft} \
 --ac-protocol=two_pc --performance-test-timeout-deadline=PT120M --deploy-monitoring=true
--- cmd/scripts/stress-consensus-test.sh
+++ cmd/scripts/stress-consensus-test.sh
@@ -3,8 +3,8 @@
 echo "Run protocol: $CONSENSUS"
 
 ./ucac perform --monitoring-namespace=rszuma \
---peers=$(python3 -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:af66ef035a7e43eec35a34c39d10adca0c1bff6a  \
---performance-test-image=ghcr.io/davenury/tests:af66ef035a7e43eec35a34c39d10adca0c1bff6a \
+--peers=$(python3 -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:3b39fa5511dd67f08fa2c7eda4a1c1fdd01a61f8  \
+--performance-test-image=ghcr.io/davenury/tests:3b39fa5511dd67f08fa2c7eda4a1c1fdd01a61f8 \
 --fixed-peersets-in-change=1 --tests-sending-strategy=delay_on_conflicts \
 --ac-protocol=two_pc --performance-test-timeout-deadline=PT120M --deploy-monitoring=true \
 --consensus-protocol=${CONSENSUS:-raft} \
