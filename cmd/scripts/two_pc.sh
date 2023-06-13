ef4229e0fae3f39a34dc999b6968dc49a61da8d7
Radosław Szuma <radoslawszuma@gmail.com>
2023-06-13 21:49:55 +0200

    Modify run_test

--- cmd/scripts/two_pc.sh
+++ cmd/scripts/two_pc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-"${SCRIPT_DIR}/../ucac" perform --monitoring-namespace=ddebowski \
+"${SCRIPT_DIR}/../ucac" perform --monitoring-namespace=rszuma \
 --peers=$(python3 -c "print('$1,' * $2, end=''); print('$1')") --test-namespace=ddebowski --application-image=ghcr.io/davenury/ucac:$(git rev-parse HEAD) \
 --performance-test-image=ghcr.io/davenury/tests:$(git rev-parse HEAD) --constant-load=5 --load-generator-type=constant \
 --fixed-peersets-in-change=2 --tests-sending-strategy=delay_on_conflicts \
